3837ce73e677ec795f3387d2fe094bf99c94917a
Tajo <gustavotajo@gmail.com>
2023-07-02 18:52:46 -0300

    weslley bixona

--- pages-sections/LandingPage-Sections/WorkSection.js
+++ pages-sections/LandingPage-Sections/WorkSection.js
@@ -1,35 +1,32 @@
 import React from "react";
-// @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-
-// @material-ui/icons
-
-// core components
 import GridContainer from "/components/Grid/GridContainer.js";
 import GridItem from "/components/Grid/GridItem.js";
 import CustomInput from "/components/CustomInput/CustomInput.js";
 import Button from "/components/CustomButtons/Button.js";
-
 import styles from "/styles/jss/nextjs-material-kit/pages/landingPageSections/workStyle.js";
 
 const useStyles = makeStyles(styles);
 
 export default function WorkSection() {
   const classes = useStyles();
+
   return (
     <div className={classes.section}>
       <GridContainer justify="center">
         <GridItem cs={12} sm={12} md={8}>
-          <h2 className={classes.title}>Fale com um consultor sobre o residencial Curuça </h2>
+          <h2 className={classes.title}>
+            Fale com um consultor sobre o residencial Curuça
+          </h2>
           <h4 className={classes.description}>
             Saiba como realizar o sonho de morar em um apartamento novo com toda a qualidade de vida que você deseja no centro de Mauá
           </h4>
-          <form action="https://formsubmit.co/emailteste@email.com" method="POST"/>
-          <input type="hidden" name="_cc" value="another@email.com"/>
-          <input type="hidden" name="_next" value=""/>
+          <form action="
+https://formsubmit.co/weslleycassio18@gmail.com
+" method="POST">
             <GridContainer>
               <GridItem xs={12} sm={12} md={6}>
-                <CustomInput
+                <input type = "text"
                   labelText="Nome Completo"
                   id="name"
                   formControlProps={{
@@ -38,7 +35,7 @@ export default function WorkSection() {
                 />
               </GridItem>
               <GridItem xs={12} sm={12} md={6}>
-                <CustomInput
+                <input type = "text"
                   labelText="E-mail"
                   id="email"
                   formControlProps={{
@@ -47,7 +44,7 @@ export default function WorkSection() {
                 />
               </GridItem>
               <GridItem xs={12} sm={12} md={6}>
-                <CustomInput
+                <input type = "text"
                   labelText="Telefone"
                   id="tel"
                   formControlProps={{
@@ -56,7 +53,9 @@ export default function WorkSection() {
                 />
               </GridItem>
               <GridItem xs={12} sm={12} md={4} className={classes.textCenter}>
-                <Button color="warning">Quero Saber Tudo !</Button>
+                <Button color="warning" type="submit">
+                  Quero Saber Tudo!
+                </Button>
               </GridItem>
             </GridContainer>
           </form>
@@ -64,4 +63,4 @@ export default function WorkSection() {
       </GridContainer>
     </div>
   );
-}
+} 
